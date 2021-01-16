64c7bdd1d468c34238fe59f281b40e7fa5bc52a4
Dannyel Kayke <dannyel_kayke@hotmail.com>
2021-01-16 00:16:47 -0300

    Ajustes login/signup e criação onboarding

---
+++ src/modules/onboarding/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Routes
+import Onboarding from './pages/Onboarding';
+
+const OnboardingModule: FC = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/onboarding" component={Onboarding} />
+      </Switch>
+    </>
+  );
+};
+
+export default OnboardingModule;
---
+++ src/modules/onboarding/pages/Personal/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const Personal: FC = () => {
+    return (
+        <></>
+    );
+}
+
+export default Personal;
