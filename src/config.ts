e304a843967448e3fbc5dd6daf935746f5e10f16
Dannyel Kayke <dannyel_kayke@hotmail.com>
2021-03-11 00:24:01 -0300

    alteração config para produção

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@ import { Config } from 'typing/interfaces';
 import { Languages } from './typing/enums';
 
 export const config: Config = {
-    environment: 'development',
+    environment: 'production',
     language: Languages.PortuguesBr,
     appName: 'Hunood Web',
-    baseUrl: process.env.REACT_APP_URL_BASE as string,
-    backendUrl: 'http://localhost:3001',
+    baseUrl: 'https://hunood-web.herokuapp.com/',
+    backendUrl: 'https://hunood-backend.herokuapp.com/',
     timeout: 5000
 };
