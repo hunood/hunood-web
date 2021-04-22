242d072b38eb35ca675ea9f7e25adc53ec602caa
Dannyel Kayke <dannyel_kayke@hotmail.com>
2021-04-21 22:50:06 -0300

    correção de bug layout signup

--- src/components/forms/SignupForm/index.tsx
+++ src/components/forms/SignupForm/index.tsx
@@ -19,7 +19,6 @@ const SignupForm: FC<SignupFormProps> = () => {
         <>
             <Form.Item
                 name="username"
-                hasFeedback
                 rules={[
                     {
                         required: true,
